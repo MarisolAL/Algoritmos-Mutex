88913dcb36b85cb5f781a9cd78437877d47acea2
Unknown <marisolamezcua@ciencias.unam.mx>
2018-09-22 10:49:44 -0500

    Codigo terminado

---
+++ practica1.c
@@ -0,0 +1,144 @@
+/*
+ * practica1.c
+ *
+ *  Created on: 2 sept. 2018
+ *      Author: marisol
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <omp.h>
+
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+
+static int turno_d = 0;
+static int flag_d[2] = {0, 0};
+
+static int turno_p = 0;
+static int flag_p[2] = {0, 0};
+
+static int local_k[2] = {0,0};
+static int b_k[2] = {0,0};
+static int turno_k[2] = {0,1};
+
+
+/*Rutina que se encarga de ejecutar la seccion critica*/
+void seccion_critica()
+{
+	int id_hilo = omp_get_thread_num();
+	if (id_hilo){
+		printf(ANSI_COLOR_GREEN "%d: Ya entre \n \033[0m"  ,id_hilo);
+	}else{
+		printf(ANSI_COLOR_BLUE "%d: Ya entre \n \033[0m"  ,id_hilo);
+	}
+	int i = 0;
+	//Hace la suma de los primeros 1000 naturales
+	while(i<1000){
+		i = i + 1;
+	}
+	if (id_hilo){
+		printf(ANSI_COLOR_GREEN "%d: Ya sali \n \033[0m"  ,id_hilo);
+	}else{
+		printf(ANSI_COLOR_BLUE "%d: Ya sali \n \033[0m"  ,id_hilo);
+	}
+}
+
+/*Rutina que representa el algoritmo de Dekker*/
+void proceso_hilo_dekker()
+{
+	int id = omp_get_thread_num();
+	//Levantamos la bandera de que esta por
+	//entrar en sus seccion critica
+	flag_d[id] = 1;
+	int otro = 1-id;
+	//Mientras la bandera del otro esta levantada
+	while(flag_d[otro])
+	{
+		//Si es el turno del otro le damos la
+		//oportunidad de salir del ciclo y ejecutar
+		//la seccion critica
+		if(turno_d == otro)
+		{
+			flag_d[id] = 0;
+			while(turno_d != id){}
+			//Cuando es su turno vuelve a levantar su
+			//bandera
+			flag_d[id] = 1;
+		}
+	}
+	seccion_critica();
+	//Le damos el turno al otro hilo
+	turno_d = otro;
+	//Bajamos la bandera
+	flag_d[id] = 0;
+}
+
+/*Rutina que representa el algoritmo de Peterson */
+void proceso_hilo_petterson()
+{
+	int id = omp_get_thread_num();
+	int otro = (id + 1)%2;
+	//Marcamos que va a entrar a la seccion critica
+	flag_p[id] = 1;
+	turno_p = otro;
+	//Espera si no es su turno y la bandera del otro hilo esta levantada
+	while(flag_p[otro] && turno_p == otro){}
+	seccion_critica();
+	//Bajamos la bandera
+	flag_p[id] = 0;
+}
+
+//Rutina que implementa el algoritmo de Kessels
+void proceso_hilo_Kessels()
+{
+	int id = omp_get_thread_num();
+	//Levantamos la bandera de que estamos por entrar a la
+	//seccion critica
+	b_k[id] = 1;
+	local_k[id] = (turno_k[1-id]+id)%2;
+	turno_k[id] = local_k[id];
+	while(!(b_k[1-id] == 0 || local_k[id] != (turno_k[1 -id] +id)%2)){}
+	//Si alguna de las dos condiciones anteriores no se cumple ejecutamos la seccion
+	seccion_critica();
+	//Acabando bajamos la bandera de que ocupamos la seccion critica
+	//Permitiendo al otro hilo usar la seccion
+	b_k[id] = 0;
+}
+
+
+int main(int argc, char** argv)
+{
+	omp_set_num_threads(2);
+	printf("Proceso con Dekker \n");
+	#pragma omp parallel
+	{
+		int j = 0;
+		while(j<20){
+			proceso_hilo_dekker();
+			j += 1;
+		}
+	}
+	omp_set_num_threads(2);
+	printf("Proceso con Petterson \n");
+	#pragma omp parallel
+	{
+		int j = 0;
+		while(j<20){
+			proceso_hilo_petterson();
+			j += 1;
+		}
+	}
+	omp_set_num_threads(2);
+	printf("Proceso con Kessels \n");
+	#pragma omp parallel
+	{
+		int j = 0;
+		while(j<20){
+			proceso_hilo_Kessels();
+			j += 1;
+		}
+	}
+	return 0;
+}
